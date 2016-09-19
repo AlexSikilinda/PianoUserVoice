be9d3ec0e5b20ff07c8694134bf0084bf61ad059
Alex Sikilinda <alex.sikilinda@gmail.com>
2016-09-19 22:39:13 +0300

    Initial commit

---
+++ PianoUserVoice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PianoUserVoice.MvcApplication" Language="C#" %>
---
+++ PianoUserVoice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PianoUserVoice.Startup))]
+namespace PianoUserVoice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
